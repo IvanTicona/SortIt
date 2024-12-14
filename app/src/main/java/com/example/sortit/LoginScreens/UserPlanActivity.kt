d5e9786b33cec2177cf36cab63e6e6910a567226
patty <pattyquisbert26@gmail.com>
2024-12-14 15:44:53 -0400

    changes

--- app/src/main/java/com/example/sortit/LoginScreens/UserPlanActivity.kt
+++ app/src/main/java/com/example/sortit/LoginScreens/UserPlanActivity.kt
@@ -1,10 +1,11 @@
-package com.example.sortit
+package com.example.sortit.LoginScreens
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sortit.R
 
 class UserPlanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
